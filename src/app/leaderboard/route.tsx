f59a7082b034ed7dced7c918dfd428c0ab053d83
Benjamin Barbeau <benjamin@barbeau.net>
2023-05-12 01:03:50 -0400

    Disable database

--- src/app/leaderboard/route.tsx
+++ src/app/leaderboard/route.tsx
@@ -1,17 +1,18 @@
 import mysql from "mysql2/promise";
 
 export async function GET() {
-  const con = await mysql.createConnection({
+  /*const con = await mysql.createConnection({
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT || '3006'),
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
-  });
+  });*/
 
   try {
-    const [results] = await con.query("SELECT * from games");
-    con.end();
+    /*const [results] = await con.query("SELECT * from games");
+    con.end();*/
+    const results = {test: "test"}
 
     return new Response (JSON.stringify(results), { 
       status: 200 
